36fe34c8ec1e9dbfc8919b529d7de0ee06ac5378
LevineLighto <hello@irimold.web.id>
2025-01-28 23:18:52 +0700

    feat: Add configs

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import laravel from "laravel-vite-plugin";
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import path from 'path'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                // Scss
+                'resources/scss/main.scss',
+
+                // JS
+                'resources/js/app.js',
+
+                // Pages
+            ],
+            refresh: true,
+        }),
+        viteReact(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js/'),
+        }
+    }
+})
