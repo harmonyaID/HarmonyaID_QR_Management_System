f31a0fe7989522387fbf45943db517a4402203d2
LevineLighto <hello@irimold.web.id>
2025-02-01 15:45:28 +0700

    feat(Input): Quantity input

---
+++ resources/js/components/inputs/QuantityInput.jsx
@@ -0,0 +1,123 @@
+import { Minus } from "@/icons/Minus"
+import { Plus } from "@/icons/Plus"
+import { useCallback, useEffect, useRef } from "react"
+
+export const QuantityInput = ({
+    id      = '',
+    name    = '',
+    value,
+    className       = '',
+    inputClassName  = '',
+    onChange        = ({name, value}) => {},
+    ...props
+}) => {
+    const inputId               = `${id}-${name}`
+    const inputRef              = useRef(null)
+    const holdTimeoutRef        = useRef()
+    const holdAccumulatorRef    = useRef(0)
+    const isHoldingRef          = useRef(false)
+
+    const handleChange = (event) => {
+        const target    = event.target
+        const newValue  = target.value
+
+        if (typeof onChange == 'function') {
+            onChange({name: name, value: newValue})
+        }
+    }
+
+    const handleClick = (addition, shouldFocus = true) => {
+        const currentValue = inputRef.current.value ? parseInt(inputRef.current.value) : 0
+        const newValue = currentValue + addition
+
+        inputRef.current.value = newValue
+        if (shouldFocus) {
+            inputRef.current.focus()
+        }
+        
+        if (typeof onChange == 'function') {
+            onChange({name: name, value: newValue})
+        }
+    }
+
+    const handleMouseDown = (addition, timeout = 600) => {
+        clearTimeout(holdTimeoutRef.current)
+        holdTimeoutRef.current = setTimeout(() => {
+            isHoldingRef.current = true
+            if (holdAccumulatorRef.current < 40) {
+                holdAccumulatorRef.current++
+            }
+            
+            const accumulator   = holdAccumulatorRef.current
+            let multiplier      = 1
+            if (accumulator >= 10 && accumulator < 20) {
+                multiplier = 10
+            } else if (accumulator >= 20 && accumulator < 40) {
+                multiplier = 100
+            } else if (accumulator >= 40) {
+                multiplier = 1000
+            }
+
+            handleClick(addition * multiplier, false)
+            handleMouseDown(addition, 100)
+        }, timeout)
+    }
+
+    const handleMouseUp = useCallback(() => {
+        if (!isHoldingRef.current) {
+            return
+        }
+
+        isHoldingRef.current = false
+        clearTimeout(holdTimeoutRef.current)
+        holdAccumulatorRef.current = 0
+        inputRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        document.addEventListener('mouseup', handleMouseUp)
+
+        return () => {
+            document.removeEventListener('mouseup', handleMouseUp)
+        }
+    }, [handleMouseUp])
+
+    return (
+        <div
+            className={`${
+                'input-quantity bg-white rounded-pill shadow-none'
+            } ${
+                'd-inline-flex align-items-stretch justify-content-between'
+            } ${
+                className
+            }`}
+        >
+            <button
+                className="d-inline-flex align-items-center justify-content-center rounded-pill"
+                onClick={() => handleClick(-1)}
+                onMouseDown={() => handleMouseDown(-1)}
+                onMouseUp={() => handleMouseUp()}
+            >
+                <Minus size={18}/>
+            </button>
+            <input
+                ref={inputRef}
+                id={inputId}
+                type="number"
+                name={name}
+                value={value}
+                onChange={handleChange}
+                className={inputClassName}
+                {...props}
+            />
+            <button
+                className="d-inline-flex align-items-center justify-content-center rounded-pill"
+                onClick={() => handleClick(1)}
+                onMouseDown={() => handleMouseDown(1)}
+                onMouseUp={() => handleMouseUp()}
+            >
+                <Plus size={18}/>
+            </button>
+        </div>
+    )
+}
---
+++ resources/js/icons/Plus.jsx
@@ -0,0 +1,17 @@
+export const Plus = ({
+    size = 64,
+    ...props
+}) => (
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 1024 1024" 
+        height={size}
+        width={size}
+        {...props}
+    >
+        <path 
+            fill="currentColor" 
+            d="M480 480V128a32 32 0 0 1 64 0v352h352a32 32 0 1 1 0 64H544v352a32 32 0 1 1-64 0V544H128a32 32 0 0 1 0-64z"
+        />
+    </svg>
+)
