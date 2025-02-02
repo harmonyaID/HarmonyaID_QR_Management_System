9bc8a32d52ff64aed00e71828dcc59298fd387a8
LevineLighto <hello@irimold.web.id>
2025-02-02 22:04:48 +0700

    feat(Helper): Add error helpers

--- app/Services/Helpers/Status/globals.php
+++ app/Services/Helpers/Status/globals.php
@@ -6,3 +6,39 @@ if (!function_exists("errDefault")) {
         error(500, "An error occurred!", $internalMsg, $status);
     }
 }
+
+if (!function_exists("errInvalid")) {
+    function errInvalid($message = "Invalid input!", $internalMsg = "", $status = null)
+    {
+        error(400, $message, $internalMsg, $status);
+    }
+}
+
+if (!function_exists("errUnauthorized")) {
+    function errUnauthorized($message = "You are not allowed to do this action!", $internalMsg = "", $status = null)
+    {
+        error(401, $message, $internalMsg, $status);
+    }
+}
+
+if (!function_exists("errNotFound")) {
+    function errNotFound(string $modelClass, $internalMsg = '')
+    {
+        if (class_exists($modelClass)) {
+            $reflection = new ReflectionClass($modelClass);
+            if ($shortName = $reflection->getShortName()) {
+                $name = preg_replace("/([A-Z][a-z])/", " $1", $shortName);
+                $name = preg_replace("/([A-Z]+)/", " $1", $name);
+                $name = preg_replace("/ +/", " ", $name);
+            } else {
+                $name = $modelClass;
+            }
+        } else {
+            $name = $modelClass;
+        }
+
+        $name = strtolower(trim($name));
+    
+        error(404, "Unable to find the specified {$name}!", $internalMsg);
+    }
+}
