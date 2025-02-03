12f7804bc9c94a35d57502f00cd0927218530328
LevineLighto <hello@irimold.web.id>
2025-02-03 21:37:24 +0700

    fix(Button): Add support for link element

--- resources/js/components/buttons/Button.jsx
+++ resources/js/components/buttons/Button.jsx
@@ -1,4 +1,12 @@
-'use client';
+import { Link } from "@inertiajs/react"
+
+const Btn = (props) => (
+    <button {...props}/>
+)
+
+const DefaultLink = (props) => (
+    <Link {...props}/>
+)
 
 export const Button = ({
     small       = false,
@@ -6,29 +14,34 @@ export const Button = ({
     disabled    = false,
     className   = '',
     text        = false,
+    link        = false,
     children,
     ...props
-}) => (
-    <button 
-        disabled={disabled}
-        className={`${
-            'btn'
-        } ${
-            text && disabled ? 'text-neutral-200 btn-text' :
-            text ? 'text-primary btn-text' :
-            outline && disabled ? 'btn-outline-neutral-200' :
-            outline ? 'btn-outline-primary' :
-            disabled ? 'btn-neutral-200 text-white' :
-            'btn-primary text-white' 
-        } ${
-            small ? 'btn-sm' : ''
-        } ${
-            className
-        }`}
-        {...props}
-    >
-        <span className="d-inline-block">
-            { children }
-        </span>
-    </button>
-)
+}) => {
+    const Component = link ? DefaultLink : Btn
+
+    return (
+        <Component 
+            disabled={disabled}
+            className={`${
+                'btn'
+            } ${
+                (text || link) && disabled ? 'text-neutral-200 btn-text' :
+                (text || link) ? 'text-primary btn-text' :
+                outline && disabled ? 'btn-outline-neutral-200' :
+                outline ? 'btn-outline-primary' :
+                disabled ? 'btn-neutral-200 text-white' :
+                'btn-primary text-white' 
+            } ${
+                small ? 'btn-sm' : ''
+            } ${
+                className
+            }`}
+            {...props}
+        >
+            <span className="d-inline-block">
+                { children }
+            </span>
+        </Component>
+    )
+}
