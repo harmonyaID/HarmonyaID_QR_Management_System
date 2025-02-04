b2f18e742bdca08678dfcad2c8b1e05174c513b9
LevineLighto <hello@irimold.web.id>
2025-02-04 22:06:27 +0700

    fix(User): Missing remember token column

--- database/migrations/2025_01_29_161330_create_users_table.php
+++ database/migrations/2025_01_29_161330_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('lastname');
             $table->string('email');
             $table->string('password');
+            $table->string('rememberToken', 100)->nullable();
 
             $table->foreignUuid('roleId')->nullable()->references('id')->on('roles');
             $table->foreignUuid('planId')->nullable()->references('id')->on('plans');
