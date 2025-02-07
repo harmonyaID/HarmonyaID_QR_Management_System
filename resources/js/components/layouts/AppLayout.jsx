1e6781e9bc6681b03ac4415ff105c00a7ddeec09
LevineLighto <hello@irimold.web.id>
2025-02-07 21:50:58 +0800

    fix(App Layout): Change body bg for app layout

--- resources/js/components/layouts/AppLayout.jsx
+++ resources/js/components/layouts/AppLayout.jsx
@@ -2,25 +2,32 @@ import { SidebarProvider } from "@/contexts/navigations/SidebarContext";
 import { Sidebar } from "../navigations/Sidebar";
 import { Navbar } from "../navigations/Navbar";
 import { Head } from "@inertiajs/react";
+import { useEffect } from "react";
 
 export const AppLayout = ({
     title = '',
     children
-}) => (
-    <>
-        <Head
-            title={title}
-        />
-        <SidebarProvider>
-            <div className="d-flex">
-                <Sidebar/>
-                <main className="flex-grow-1">
-                    <Navbar title={title}/>
-                    <div className="m-2 px-3 py-4">
-                        { children }
-                    </div>
-                </main>
-            </div>
-        </SidebarProvider>
-    </>
-)
+}) => {
+    useEffect(() => {
+        document.body.classList.add('bg-neutral-50')
+    }, [])
+
+    return (
+        <>
+            <Head
+                title={title}
+            />
+            <SidebarProvider>
+                <div className="d-flex">
+                    <Sidebar/>
+                    <main className="flex-grow-1">
+                        <Navbar title={title}/>
+                        <div className="m-2 px-3 py-4">
+                            { children }
+                        </div>
+                    </main>
+                </div>
+            </SidebarProvider>
+        </>
+    )
+}
