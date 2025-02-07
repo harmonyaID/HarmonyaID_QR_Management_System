cd6e562e5622015d330c0d7ce598b49d99e5f40f
LevineLighto <hello@irimold.web.id>
2025-02-07 21:47:23 +0800

    feat: Offcanvas component

---
+++ resources/js/components/offcanvas/Offcanvas.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef } from "react"
+import { Offcanvas as BsOffcanvas } from "bootstrap"
+
+export const Offcanvas = ({
+    title= '',
+    open    = false,
+    onHide  = () => {},
+    children
+}) => {
+    const offcanvasRef = useRef(null)
+    const bsOffCanvasRef = useRef(null)
+
+    useEffect(() => {
+        if (typeof onHide != 'function' || !offcanvasRef.current) {
+            return
+        }
+
+        console.count('Reattach onhide');
+
+        offcanvasRef.current?.addEventListener('hide.bs.offcanvas', onHide)
+
+        return () => {
+            offcanvasRef.current?.removeEventListener('hide.bs.offcanvas', onHide)
+        }
+    }, [onHide]);
+
+    useEffect(() => {
+        bsOffCanvasRef.current = new BsOffcanvas(offcanvasRef.current)
+    }, [])
+
+    useEffect(() => {
+        if (open) {
+            bsOffCanvasRef.current.show()
+        }
+    }, [open])
+
+    return (
+        <div 
+            ref={offcanvasRef}
+            className={`${
+                "offcanvas offcanvas-end"
+            }`}
+            tabIndex="-1"
+        >
+            <div className="offcanvas-header">
+                <h5 
+                    className="offcanvas-title"
+                >
+                    { title }
+                </h5>
+                <button 
+                    type="button" 
+                    className="btn-close" 
+                    data-bs-dismiss="offcanvas" 
+                    aria-label="Close"
+                ></button>
+            </div>
+            <div className="offcanvas-body">
+                { children }
+            </div>
+        </div>
+    )
+}
