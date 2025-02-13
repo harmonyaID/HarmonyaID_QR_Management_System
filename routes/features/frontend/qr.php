b8e4c0badce4368467d7af4410f0f0c689d86a87
LevineLighto <hello@irimold.web.id>
2025-02-13 23:55:17 +0800

    fix(Qr): Change frontend url from qrs to qr-codes

--- routes/features/frontend/qr.php
+++ routes/features/frontend/qr.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Frontend\Qr\QrController;
 use App\Http\Controllers\Frontend\Qr\QrSettingController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('qrs')
+Route::prefix('qr-codes')
     ->as('qrs.')
     ->group(function () {
+
+        Route::get('create', [QrController::class, 'create'])->name('create');
 
         Route::get('settings', [QrSettingController::class, 'index'])->name('settings');
 
